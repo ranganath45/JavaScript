c27d8386c78d332981fc2dfb91925b5caf0e6999
Ranganath <109332542+ranganath45@users.noreply.github.com>
2023-07-13 16:52:07 +0530

    Add files via upload

---
+++ Functions Practice/script.js
@@ -0,0 +1,85 @@
+"use strict";
+/*
+const a = [];
+const flightBooking = function (
+  flightNum,
+  noOfPassengers = 2,
+  price = 450 * noOfPassengers
+) {
+  const booking = {
+    flightNum,
+    noOfPassengers,
+    price,
+  };
+  console.log(booking);
+  a.push(booking);
+};
+flightBooking(2);
+flightBooking(2, 5, 1000);
+flightBooking(3, 7);
+flightBooking(5, undefined, 800);
+
+
+const fligt = "IA45";
+const raghu = {
+  name: "Raghu",
+  passport: 4567901230,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "IA245";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 4567901230) alert("Checked In");
+  else alert("Wrong Passport Number");
+};
+checkIn(fligt, raghu);
+console.log(fligt, raghu);
+
+const newFlight = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+newFlight(raghu);
+checkIn(fligt, raghu);
+console.log(raghu);c
+
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const firstWordUpper = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const transform = function (str, fn) {
+  console.log(`original string ${str}`);
+  console.log(`Transformed string ${fn(str)}`);
+
+  console.log(`transformed by ${fn.name}`);
+};
+transform("Raghu Konanki", firstWordUpper);
+transform("raghu Konanki", oneWord);
+
+
+// ----------- Capitalize each word
+const capitalName = function (name) {
+  const names = name.split(" ");
+  const newNameUpper = [];
+  for (const n of names) {
+    //newNameUpper.push(n[0].toUpperCase() + n.slice(1));
+    newNameUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(newNameUpper.join(" "));
+};
+capitalName("raghu konanki");
+
+// ----------- lower, upper,slicing
+const name = function (raghu) {
+  const nameLower = raghu.toLowerCase();
+  const nameCorrect = nameLower[0].toUpperCase() + nameLower.slice(1);
+  console.log(nameCorrect);
+};
+name("RagHu KONanki");
+*/
